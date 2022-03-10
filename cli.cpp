30d443c808cf3f282573398f32581224575538c1
chzesa <chzesa@gmail.com>
2022-03-10 08:47:23 +0200

    Remove filesystem import from header

--- cli.cpp
+++ cli.cpp
@@ -1,6 +1,9 @@
 #define CZSFP_IMPLEMENTATION
 #include "filepack.hpp"
 
+#include <filesystem>
+#include <vector>
+
 int main(int argc, char** argv)
 {
 	if (argc != 3)
@@ -8,5 +11,21 @@ int main(int argc, char** argv)
 
 	uint64_t threads = 32;
 	uint64_t memory = uint64_t(1024) * 1024 * 128 * threads;
-	czsfp::FilePack::create(argv[2], argv[1], threads, memory);
+	std::vector<std::string> file_names;
+
+	for (auto const& dir_entry : std::filesystem::recursive_directory_iterator(argv[1]))
+	{
+		if (!dir_entry.is_regular_file())
+			continue;
+
+		file_names.push_back(dir_entry.path());
+	}
+
+	const char** array = new const char*[file_names.size()];
+
+	for (size_t i = 0; i < file_names.size(); i++)
+		array[i] = file_names[i].c_str();
+
+	czsfp::FilePack::create(argv[2], argv[1], file_names.size(), array, threads, memory);
+	delete array;
 }
--- filepack.hpp
+++ filepack.hpp
@@ -24,7 +24,7 @@ struct FilePack
 	FilePack(const char* path);
 	FileQuery get(const char* pack_path) const;
 	static FilePack load(const char* pack_path);
-	static void create(const char* pack_path, const char* asset_folder_path, uint64_t threads, uint64_t memory);
+	static void create(const char* pack_path, const char* asset_path_prefix, uint64_t file_count, const char** file_paths, uint64_t threads, uint64_t memory);
 	static void update(const char* pack_path, uint64_t manifest_count, FileManifest* manifests, uint64_t threads, uint64_t memory);
 private:
 	std::unordered_map<std::string, FileQuery> locations;
@@ -40,7 +40,6 @@ private:
 #include <algorithm>
 #include <atomic>
 #include <cstring>
-#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
@@ -81,6 +80,15 @@ struct PackManifest
 
 	uint64_t names_size() { return info_offset - name_offset; }
 	uint64_t infos_size() { return sizeof (FileManifest) * file_count; }
+};
+
+struct Builder
+{
+	uint64_t file_count;
+	const char** file_paths;
+	const char* path_prefix;
+	const char* output;
+	std::vector<FileManifest> manifests;
 };
 
 FileQuery FilePack::get(const char* filename) const
@@ -147,34 +155,34 @@ void copy_file(FILE* output, const FileManifest& info, const char* path, char* b
 	fclose(input);
 }
 
-void copy_thread(std::atomic_uint64_t* index, const char* destination_path, std::vector<std::string>* paths, std::vector<FileManifest>* file_infos, char* buffer, uint64_t buffer_block_size)
+void copy_thread(Builder* builder, std::atomic_uint64_t* index, char* buffer, uint64_t buffer_block_size)
 {
-	FILE* output = fopen(destination_path, "r+b");
+	FILE* output = fopen(builder->output, "r+b");
 
 	if (!output)
 	{
-		std::cout << "Failed to open ostream to " << destination_path << std::endl;
+		std::cout << "Failed to open ostream to " << builder->output << std::endl;
 		return;
 	}
 
-	for (uint64_t i = (*index)++; i < file_infos->size(); i = (*index)++)
+	for (uint64_t i = (*index)++; i < builder->file_count; i = (*index)++)
 	{
-		copy_file(output, (*file_infos)[i], (*paths)[i].c_str(), buffer, buffer_block_size);
+		copy_file(output, builder->manifests[i], builder->file_paths[i], buffer, buffer_block_size);
 	}
 
 	fclose(output);
 }
 
-void check_result(std::vector<FileManifest>& infos, std::vector<std::string>& paths, const char* asset_folder_path, const char* pack_path, uint64_t threads, uint64_t memory)
+void check_result(std::vector<FileManifest>& infos,  uint64_t file_count, const char** file_paths, const char* asset_folder_path, const char* pack_path, uint64_t threads, uint64_t memory)
 {
 	FilePack pack(pack_path);
 	uint64_t file_name_prefix_length = std::string(asset_folder_path).length();
 	char* buffer = reinterpret_cast<char*>(malloc(memory));
 	std::ifstream pack_file(pack_path, std::ios::binary | std::ios::in);
 
-	for (int i = 0; i < paths.size(); i++)
+	for (int i = 0; i < file_count; i++)
 	{
-		auto path = paths[i].substr(file_name_prefix_length);
+		auto path = std::string(file_paths[i]).substr(file_name_prefix_length);
 		auto info = infos[i];
 		FileQuery query = pack.get(path.c_str());
 
@@ -197,7 +205,7 @@ void check_result(std::vector<FileManifest>& infos, std::vector<std::string>& pa
 		}
 
 		uint64_t buffer_size = memory / 2;
-		std::ifstream original_file(paths[i], std::ios::binary | std::ios::in);
+		std::ifstream original_file(file_paths[i], std::ios::binary | std::ios::in);
 		pack_file.seekg(info.offset);
 
 		for (uint64_t block = 0; block < info.size; block += buffer_size)
@@ -223,47 +231,55 @@ void check_result(std::vector<FileManifest>& infos, std::vector<std::string>& pa
 	pack_file.close();
 }
 
-void FilePack::create(const char* pack_path, const char* asset_folder_path, uint64_t threads, uint64_t memory)
+void FilePack::create(const char* pack_path, const char* asset_path_prefix, uint64_t file_count, const char** file_paths, uint64_t threads, uint64_t memory)
 {
-	std::string origin_path_str = std::string(asset_folder_path);
+	if (file_count == 0)
+		return;
+
+	Builder builder;
+	builder.file_count = file_count;
+	builder.file_paths = file_paths;
+	builder.path_prefix = asset_path_prefix;
+	builder.output = pack_path;
+
+	std::string origin_path_str = std::string(asset_path_prefix);
 
 	std::vector<std::string> names;
-	std::vector<std::string> paths;
-	std::vector<FileManifest> file_infos;
 
 	uint64_t offset = 0;
 	uint64_t name_offset = 0;
 	uint64_t total_size = sizeof(PackManifest);
 
-	for (auto const& dir_entry : std::filesystem::recursive_directory_iterator(origin_path_str))
+	for (int i = 0; i < file_count; i++)
 	{
-		if (!dir_entry.is_regular_file())
-			continue;
-
-		std::string filename = std::string(dir_entry.path());
+		std::string filename = std::string(file_paths[i]);
 		std::string substr = filename.substr(origin_path_str.length(), filename.length() - origin_path_str.length());
 
 		names.push_back(substr);
-		paths.push_back(std::filesystem::path(dir_entry).string());
 
 		FileManifest info;
 		info.offset = offset;
-		info.size = std::filesystem::file_size(dir_entry);
+
+		auto handle = fopen(file_paths[i], "rb");
+		fseek(handle, 0, SEEK_END);
+		info.size = ftell(handle);
+		fclose(handle);
+
 		info.name_length = substr.length();
 		info.name_offset = name_offset;
-		file_infos.push_back(info);
+		builder.manifests.push_back(info);
 
 		offset += info.size;
 		name_offset += info.name_length;
 		total_size += info.size + info.name_length + sizeof info;
 	}
 
-	uint64_t thread_count = std::min(uint64_t(threads), uint64_t(file_infos.size()));
+	uint64_t thread_count = std::min(threads, file_count);
 
 	std::cout
 		<< "Generating output file"
 		<< "\n\tTotal size: " << total_size
-		<< "\n\tFile count: " << file_infos.size()
+		<< "\n\tFile count: " << file_count
 		<< "\nUsing " << thread_count << " threads"
 		<< "\nUsing " << memory << " bytes of memory"
 		<< std::endl;
@@ -286,16 +302,14 @@ void FilePack::create(const char* pack_path, const char* asset_folder_path, uint
 	// Spawn threads for copying file contents
 	std::thread handles[thread_count];
 	uint64_t buffer_block_size = memory / threads;
-	std:: atomic_uint64_t index = 0;
+	std:: atomic_uint64_t index(0);
 	char* buffer = reinterpret_cast<char*>(malloc(memory));
 
 	for (uint64_t i = 0; i < thread_count; i++)
-		handles[i] = std::thread(copy_thread, &index, pack_path, &paths, &file_infos, buffer + (i * buffer_block_size), buffer_block_size);
+		handles[i] = std::thread(copy_thread, &builder, &index, buffer + (i * buffer_block_size), buffer_block_size);
 
 	// Write EoF data
-	FileManifest last_file;
-	if (file_infos.size() > 0)
-		last_file = file_infos[file_infos.size() - 1];
+	FileManifest last_file = builder.manifests[file_count - 1];
 
 	uint64_t eof_block_size = total_size - last_file.size - last_file.offset;
 	char* data = reinterpret_cast<char*>(malloc(eof_block_size));
@@ -309,15 +323,15 @@ void FilePack::create(const char* pack_path, const char* asset_folder_path, uint
 	}
 
 	// Copy file info block
-	uint64_t file_info_block_size = sizeof(FileManifest) * file_infos.size();
-	memcpy(ptr, file_infos.data(), file_info_block_size);
+	uint64_t file_info_block_size = sizeof(FileManifest) * file_count;
+	memcpy(ptr, builder.manifests.data(), file_info_block_size);
 	ptr += file_info_block_size;
 
 	// File Pack manifest
 	PackManifest manifest;
 	manifest.name_offset = last_file.size + last_file.offset;
 	manifest.info_offset = manifest.name_offset + last_file.name_length + last_file.name_offset;
-	manifest.file_count = file_infos.size();
+	manifest.file_count = file_count;
 	manifest.version = CZSFP_VERSION;
 	memcpy(ptr, &manifest, sizeof manifest);
 	ptr += sizeof manifest;
@@ -334,7 +348,7 @@ void FilePack::create(const char* pack_path, const char* asset_folder_path, uint
 		handles[i].join();
 
 	free(buffer);
-	check_result(file_infos, paths, asset_folder_path, pack_path, threads, memory);
+	check_result(builder.manifests, file_count, file_paths, asset_path_prefix, pack_path, threads, memory);
 }
 
 } //namespace czsfp
